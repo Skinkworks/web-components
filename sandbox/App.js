b9ceecc9e0b0526de062255b6d6f697a6e6ee674
Patrick Williams <pcw0002@auburn.edu>
2018-08-26 14:14:15 -0500

    Most important Updates

--- sandbox/App.js
+++ sandbox/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header" style={{backgroundColor: 'grey'}}>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Skink Sandbox</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
